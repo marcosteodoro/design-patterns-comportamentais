d81e251a22d1783fb3701870c04f29cc440263f5
Marcos Teodoro <marcosvinici_us@live.com>
2020-09-03 01:00:32 -0300

    Implementa cálculo impostos com duas alíquotas utilizando o Template Method.
    O Template Method é um padrão de projeto comportamental que define o esqueleto de um algoritmo na superclasse mas deixa as subclasses sobrescreverem etapas específicas do algoritmo sem modificar sua estrutura.
    
    https://refactoring.guru/pt-br/design-patterns/template-method

--- tests/CalculadoraDeImpostosTest.php
+++ tests/CalculadoraDeImpostosTest.php
@@ -5,6 +5,8 @@ namespace Tests;
 use Alura\DesignPattern\CalculadoraDeImpostos;
 use Alura\DesignPattern\Impostos\Icms;
 use Alura\DesignPattern\Impostos\Iss;
+use Alura\DesignPattern\Impostos\Icpp;
+use Alura\DesignPattern\Impostos\Ikcv;
 use Alura\DesignPattern\Orcamento;
 use PHPUnit\Framework\TestCase;
 
@@ -37,5 +39,40 @@ class CalculadoraDeImpostosTest extends TestCase
         $orcamento->valor = 100;
 
         $this->assertEquals(6, $calculadora->calcula($orcamento, new Iss()));
+    }
+
+    /**
+     * @test
+     */
+    public function testaCalculoImpostoIcpp()
+    {
+        $calculadora = new CalculadoraDeImpostos();
+        $orcamento = new Orcamento();
+
+        $orcamento->valor = 600;
+
+        $this->assertEquals(18, $calculadora->calcula($orcamento, new Icpp()));
+
+        $orcamento->valor = 500;
+
+        $this->assertEquals(10, $calculadora->calcula($orcamento, new Icpp()));
+    }
+
+    /**
+     * @test
+     */
+    public function testaCalculoImpostoIkcv()
+    {
+        $calculadora = new CalculadoraDeImpostos();
+        $orcamento = new Orcamento();
+
+        $orcamento->valor = 400;
+        $orcamento->quantidadeItens = 4;
+
+        $this->assertEquals(16, $calculadora->calcula($orcamento, new Ikcv()));
+
+        $orcamento->valor = 200;
+
+        $this->assertEquals(5, $calculadora->calcula($orcamento, new Ikcv()));
     }
 }
