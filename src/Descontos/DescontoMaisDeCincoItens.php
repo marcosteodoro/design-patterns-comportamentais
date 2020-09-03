76bfad8268e95586906e5d383bd00bf57389076d
Marcos Teodoro <marcosvinici_us@live.com>
2020-09-03 00:12:43 -0300

    Corrige regras dos descontos

--- src/Descontos/DescontoMaisDeCincoItens.php
+++ src/Descontos/DescontoMaisDeCincoItens.php
@@ -8,8 +8,8 @@ class DescontoMaisDeCincoItens extends Desconto
 {
     public function calculaDesconto(Orcamento $orcamento): float
     {
-        if ($orcamento->valor > 500) {
-            return $orcamento->valor *  0.05;
+        if ($orcamento->quantidadeItens > 5) {
+            return $orcamento->valor *  0.1;
         }
 
         return $this->proximoDesconto->calculaDesconto($orcamento);
