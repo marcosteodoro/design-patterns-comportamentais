fa4dcb33942c2253461cf628bae7800a1f453e15
Marcos Teodoro <marcosvinici_us@live.com>
2020-09-13 19:24:10 -0300

    Implementação das ações após a geração do pedido com o padrão Observer
    O Observer é um padrão de projeto comportamental que permite que você defina um mecanismo de assinatura para notificar múltiplos objetos sobre quaisquer eventos que aconteçam com o objeto que eles estão observando.
    
    https://refactoring.guru/pt-br/design-patterns/observer

--- src/GerarPedidoHandler.php
+++ src/GerarPedidoHandler.php
@@ -2,8 +2,23 @@
 
 namespace Alura\DesignPattern;
 
+use Alura\DesignPattern\AcoesAoGerarPedido\AcaoAposGerarPedido;
+use Alura\DesignPattern\AcoesAoGerarPedido\CriarPedidoNoBanco;
+use Alura\DesignPattern\AcoesAoGerarPedido\EnviarPedidoPorEmail;
+use Alura\DesignPattern\AcoesAoGerarPedido\LogGerarPedido;
+
 class GerarPedidoHandler
 {
+    /**
+     * @var AcaoAposGerarPedido
+     */
+    private array $acoesAposGerarPedido = [];
+
+    public function adicionarAcaoAoGerarPedido(AcaoAposGerarPedido $acaoAposGerarPedido)
+    {
+        $this->acoesAposGerarPedido[] = $acaoAposGerarPedido;
+    }
+
     public function execute(GerarPedido $gerarPedido)
     {
         $orcamento = new Orcamento();
@@ -14,7 +29,10 @@ class GerarPedidoHandler
         $pedido = new Pedido();
         $pedido->dataFinalizacao = new \DateTimeImmutable();
         $pedido->nomeCliente = $gerarPedido->getNomeCliente();
-
         $pedido->orcamento = $orcamento;
+
+        foreach ($this->acoesAposGerarPedido as $acaoAposGerarPedido) {
+            $acaoAposGerarPedido->executaAcao($pedido);
+        }
     }
 }
